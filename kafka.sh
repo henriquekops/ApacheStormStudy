90c5519782a69b4855b3704379e3c68d1759e169
Henrique Kops <henriquekops@localhost.localdomain>
2021-11-15 01:28:36 -0300

    add kafka and zookeeper shellscripts

---
+++ kafka.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kafka_2.12-3.0.0/bin/kafka-server-start.sh kafka_2.12-3.0.0/config/server.properties
---
+++ zoo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kafka_2.12-3.0.0/bin/zookeeper-server-start.sh kafka_2.12-3.0.0/config/zookeeper.properties
